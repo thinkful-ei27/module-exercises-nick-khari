4a185404c53e094c1763523a5d0d4382e66eafbe
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-10 14:38:29 -0600

    Put functions validateName and create within the const item. Tested them. Validate name now throws a new TypeError. Exercise one is completed.

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,23 +1,20 @@
 'use strict';
 const Item = (function(){
-  const foo = 'bar';
-  return {};
+  return {validateName, create};
 }());
 
-function validateName(name) {
-    try{
-        if(name === '' || name === undefined){
-          throw new Error;
-        }
-    } catch(error){alert('Name does not exist');}
-}
+function validateName(input) {
+  if(input === '' || input === undefined){
+    throw new TypeError('Name does not exist');
+  }
+} 
+
 
 function create(name) {
-    let obj = {id: cuid(),
+  return {id: cuid(),
     name: name,
     checked: false
   };
-    return obj;
 }
 
-console.log(create('Bilbo Baggins'));
+// eslint-disable-next-line no-console
