2b27d6c1f57436c659a5c9fb1676282b4fcdd640
kriviears <kriviears@gmail.com>
2018-12-10 15:52:34 -0500

    Adding to list using the item.js module

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -56,7 +56,14 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    //store.items.push({ id: cuid(), name: itemName, checked: false });
+    try{
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+      render();
+    }catch(e){
+      console.log(`Can not add ${itemName}: ${e.message}`);
+    }
   }
   
   function handleNewItemSubmit() {
