d1d50d7cdfd92f762c764699d765b7c131c962ca
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-10 15:59:06 -0600

    findAndToggleChecked and findAndUpdateName functions added. Currently working.

--- scripts/store.js
+++ scripts/store.js
@@ -10,8 +10,50 @@ const store = (function(){
   ];
   let hideCheckedItems = false;
   let searchTerm = '';
-    
+  let findById = function(input){
+    function isId(item){
+      return input === item.id;
+    }
+    return store.items.find(isId);
+  };
+  let addItem = function(name){
+    try{
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+      shoppingList.render();
+    } catch(e){alert(e.message);}
+  };
+  let findAndToggleChecked = function(id){
+    this.findById(id).checked = !this.findById(id).checked;
+  };
+  let findAndUpdateName = function(id, newName){
+    try{
+      Item.validateName(newName);
+      this.findById(id).name = newName;
+      shoppingList.render();
+    } catch(e){console.log(`Cannot update name: ${e.message}`);}
+  };
   return {items,
     hideCheckedItems,
-    searchTerm};
+    searchTerm, findById, addItem, findAndToggleChecked, findAndUpdateName};
 }());
+
+
+
+
+/*
+function findById(input){
+  function isId(item){
+    return input === item.id;
+  }
+  return store.items.find(isId);
+}
+
+function addItem(name){
+  try{
+    Item.validateName(name);
+    store.items.push(Item.create(name));
+    shoppingList.render();
+  } catch(e){alert(e.message);}
+}
+*/
